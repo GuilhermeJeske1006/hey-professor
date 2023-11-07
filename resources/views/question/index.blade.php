d2600d7cfddfc1089c373b01f823904c42c83d50
Guilherme jeske <guilhermeieski@gmail.com>
2023-11-07 12:07:02 -0300

    hey-15: it should be able to list all question created by me

---
+++ resources/views/question/index.blade.php
@@ -0,0 +1,28 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Minhas perguntas') }}
+        </h2>
+    </x-slot>
+
+    <x-container>
+        <x-form method="POST" :action="route('question.store')">
+            <div class="mb-4">
+                <x-textarea label="Sua Pergunta" name="question"/>
+            </div>
+            <x-btn.primary type="submit">Save</x-btn.primary>
+            <x-btn.reset type="reset">Cancel</x-btn.reset>
+    </x-form>
+
+    <hr class="border-gray-700 border-dashed my-4">
+
+    <div class="dark:text-gray-400 uppercase font-bold mb-1">Minhas de perguntas</div>
+
+    <div class="dark:text-gray-400 space-y-4">
+        @foreach ($questions as $item)
+            <x-question :question="$item"></x-question>
+        @endforeach
+    </div>
+    </x-container>
+        
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::get('/question', [QuestionController::class, 'index'])->name('question.index');
     Route::post('/question/store', [QuestionController::class, 'store'])->name('question.store');
     Route::post('/question/like/{question}', Question\LikeController::class)->name('question.like');
     Route::post('/question/unlike/{question}', Question\UnlikeController::class)->name('question.unlike');
