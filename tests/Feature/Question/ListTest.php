aabe23094f2f71cb415e109a91403594d702e960
Guilherme jeske <guilhermeieski@gmail.com>
2023-11-13 18:14:53 -0300

    hey-16: it should paginate the result

--- tests/Feature/Question/ListTest.php
+++ tests/Feature/Question/ListTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\{Question, User};
+use Illuminate\Pagination\LengthAwarePaginator;
 
 use function Pest\Laravel\{actingAs, get};
 
@@ -22,4 +23,15 @@ it('Should list all the questions', function () {
     foreach ($questions as $q) {
         $response->assertSee($q->question);
     }
+});
+
+it('should paginate the result', function () {
+
+    // Arrange :: preparar
+    $user = User::factory()->create();
+    actingAs($user);
+    $questions = Question::factory()->count(20)->create();
+
+    get(route('dashboard'))
+        ->assertViewHas('questions', fn ($value) => $value instanceof LengthAwarePaginator);
 });
